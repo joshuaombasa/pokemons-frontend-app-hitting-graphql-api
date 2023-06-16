cacaeaebdb9bcbe9a2b73f6fdaa6e419e099cc91
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-06-16 22:24:32 +0300

    added query file

---
+++ src/graphql/get-pokemons.js
@@ -0,0 +1,18 @@
+import { useQuery, gql } from '@apollo/client'
+
+
+export  const GET_POKEMONS = gql `
+{
+  pokemons(first:9){
+    id
+    name
+    image
+    attacks{
+      special{
+        name
+        damage
+      }
+    }
+  }
+}
+`
