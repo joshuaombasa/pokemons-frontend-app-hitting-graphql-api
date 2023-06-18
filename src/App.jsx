03c48c24c80f02c71e78dc9cd7611ad6be126d3b
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-06-18 22:32:19 +0300

    romoved redundat code

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
-import { useQuery, gql } from '@apollo/client'
+
 import './App.css'
 import PokemonsContainer from './containers/PokemonsContainer'
 
 
-
 function App() {
- 
-
 
   return (
     <>
